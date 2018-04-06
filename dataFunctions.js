22078d3471e977b6c3373e1e231a4e3b778e8d6a
CreightonKasey <klm88849@creighton.edu>
2018-04-06 17:18:11 -0500

    saving progress

---
+++ dataFunctions.js
@@ -0,0 +1,16 @@
+//get the objects from the programData.js
+
+//grab an object's name
+
+//gram an objects description
+
+// grab the objects formats > the array then dig into array
+
+
+// /---------------------------------------
+// Utility Functions
+//---------------------------------------
+//adds a space between words
+function space(x) {
+    return x.replace(/([a-z])([A-Z])/g, "$1 $2");
+}
